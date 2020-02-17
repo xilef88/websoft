28f0797a9dcb6cfc4b19c276b6d3935b1b9a2c42
xilef88 <37808696+xilef88@users.noreply.github.com>
2020-02-17 13:25:26 +0100

    fixed problem with school.js
    fixed problem with school.js

--- work/report/js/school.js
+++ work/report/js/school.js
@@ -1,5 +1,4 @@
     function createFetch() {
-      'use strict';
 
         fetch("data/1290.json")
         //fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1290')
@@ -11,7 +10,6 @@
            let fetchSchool = myJson.Skolenheter
 
 
-               //Values for HTML-header
                var col = [];
                for (var i = 0; i < fetchSchool.length; i++) {
                    for (var key in fetchSchool[i]) {
@@ -21,19 +19,16 @@
                    }
                }
 
-               //Creates a dynamic table
                var table = document.createElement("table");
 
-               //Creates HTML-table header row
-               var tr = table.insertRow(-1);                   // Table row
+               var tr = table.insertRow(-1);
 
                for (var i = 0; i < col.length; i++) {
-                   var th = document.createElement("th");      // Table header
+                   var th = document.createElement("th");
                    th.innerHTML = col[i];
                    tr.appendChild(th);
                }
 
-               //Adds json to the rows
                for (var i = 0; i < fetchSchool.length; i++) {
 
                    tr = table.insertRow(-1);
@@ -44,7 +39,6 @@
                    }
                }
 
-               // Adds created table with json-data to a container
                var divContainer = document.getElementById("showData");
                divContainer.innerHTML = "";
                divContainer.appendChild(table);
